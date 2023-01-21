fc1a1c8656bbf5fae89648af7f909d41f348405e
Joonsoo Jeon <giyeok7@gmail.com>
2023-01-22 02:12:47 +0900

    version up to 0.1.0

--- bibix-base/src/main/kotlin/com/giyeok/bibix/base/Constants.kt
+++ bibix-base/src/main/kotlin/com/giyeok/bibix/base/Constants.kt
@@ -4,6 +4,8 @@ object Constants {
   const val BIBIX_GIT_URL = "https://github.com/Joonsoo/bibix.git"
 //  const val BIBIX_GIT_URL = "file:///home/joonsoo/Documents/workspace/bibix"
 
+  const val BIBIX_PLUGINS_GIT_URL = "https://github.com/Joonsoo/bibix-plugins.git"
+
   // 현재 bibix 버전
-  const val BIBIX_VERSION = "0.0.4"
+  const val BIBIX_VERSION = "0.1.0"
 }
